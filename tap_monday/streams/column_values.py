039b57cb3bdd621415a4c086cc6d38c4037a4016
atttiwari <atul.tiwari@qlik.com>
2025-08-11 16:54:10 +0530

    SAC-28355: Updated for dict get method to extract parent id

--- tap_monday/streams/column_values.py
+++ tap_monday/streams/column_values.py
@@ -26,8 +26,8 @@ class ColumnValues(FullTableStream):
     def modify_object(self, record: Dict, parent_record: Dict = None) -> Dict:
         """Modify the record before writing to the stream."""
         record = super().modify_object(record, parent_record)
-        record["item_id"] = parent_record["id"]
-        record["board_id"] = parent_record["board_id"]
+        record["item_id"] = parent_record.get("id")
+        record["board_id"] = parent_record.get("board_id")
         return record
 
     def parse_raw_records(self, raw_data: Any) -> List[Dict]:
