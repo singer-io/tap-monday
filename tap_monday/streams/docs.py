b256e1a2125b9599f7d150bba9b64c06f3eee751
Atul Tiwari <atul.tiwari@qlik.com>
2025-08-13 12:19:41 +0530

    SAC-28373 Update Bookmarking & Pagination (#3)
    * SAC-28355: Implement Sync logic for all streams
    * SAC-28373: Updated bookmarking and added pagination for parent-child streams

--- tap_monday/streams/docs.py
+++ tap_monday/streams/docs.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from singer import get_logger
 from tap_monday.streams.abstracts import FullTableStream
 
@@ -11,9 +12,18 @@ class Docs(FullTableStream):
     replication_keys = []
     data_key = "data.docs"
     object_to_id = {"created_by": "creator"}
-    root_field = "docs"
+    root_field = "docs(limit:{limit}, page:{page})"
+    page_size = 300
+    pagination_supported = True
     extra_fields = {
         "created_by": ["id"]
         }
     excluded_fields = ["creator_id"]
+
+    def update_data_payload(self, graphql_query: str = None, parent_obj: Dict = None, **kwargs) -> None:
+        """Update JSON body for GraphQL API. Injects query string if provided."""
+        page = kwargs.get("page", 1)
+        root_field = self.root_field.format(limit=self.page_size, page=page)
+        graphql_query = self.get_graphql_query(root_field)
+        super().update_data_payload(graphql_query=graphql_query, parent_obj=parent_obj, **kwargs)
 
