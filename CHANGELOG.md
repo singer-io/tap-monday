dd7d5ceb985a74ab0ec2a3079d6065c102c7b0b1
akkumar-qlik <akarsh.kumar@qlik.com>
2026-04-02 11:53:38 +0530

    Added requested changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+## 0.1.0
+  * Updated python version. [#14](https://github.com/singer-io/tap-monday/pull/14)
+  * Upgraded singer library version to latest.
+
 ## 0.0.1
   * Initial Commit
+  * Updated python version. [#14](https://github.com/singer-io/tap-monday/pull/14)
--- setup.py
+++ setup.py
@@ -4,27 +4,33 @@ from setuptools import setup, find_packages
 
 
 setup(name="tap-monday",
-      version="0.0.1",
+      version="0.1.0",
       description="Singer.io tap for extracting data from Monday API",
       author="Stitch",
       url="http://singer.io",
       classifiers=["Programming Language :: Python :: 3 :: Only"],
       py_modules=["tap_monday"],
       install_requires=[
-        "singer-python==6.1.1",
-        "requests==2.32.4",
-        "backoff==2.2.1",
-        "parameterized==0.9.0"
+          "singer-python==6.8.0",
+          "requests==2.32.5",
+          "backoff==2.2.1",
       ],
-      extras_require={'dev': ['coverage', 'pylint', 'pytest', 'ipdb']},
+      extras_require={
+          'dev': [
+              'coverage',
+              'pylint',
+              'pytest',
+              'ipdb',
+              'parameterized'
+          ]
+        },
       entry_points="""
           [console_scripts]
           tap-monday=tap_monday:main
       """,
       packages=find_packages(),
-      package_data = {
+      package_data={
           "tap_monday": ["schemas/*.json"],
       },
       include_package_data=True,
-)
-
+      )
